e6b346882b7416c9d9957f0757f2f85077963582
vavill <kxzxxx@gmail.com>
2015-09-25 17:22:07 +0800

    init

--- app/src/main/java/me/myfilemanager/Activity/MainActivity.java
+++ app/src/main/java/me/myfilemanager/Activity/MainActivity.java
@@ -2,23 +2,38 @@ package me.myfilemanager.Activity;
 
 
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Filter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.spazedog.lib.rootfw4.RootFW;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
 
 import me.myfilemanager.Custom.CustomDrawer;
 import me.myfilemanager.Fragment.NavigationDrawerFragment;
 import me.myfilemanager.NavigationDrawerCallbacks;
 import me.myfilemanager.R;
-import me.myfilemanager.Utils.UpdateList;
+import me.myfilemanager.Adapter.AdapterDetailedList;
+import me.myfilemanager.Utils.AlphanumComparator;
 
 
 //TODO show file list
@@ -27,13 +42,15 @@ import me.myfilemanager.Utils.UpdateList;
 public class MainActivity extends AppCompatActivity implements NavigationDrawerCallbacks, AdapterView.OnItemClickListener {
     private String TAG = MainActivity.class.getSimpleName();
 
-    public String currentFolder;
+    public static String currentFolder;
 
     private Toolbar ab;
 
     private NavigationDrawerFragment mNavigationDrawerFragment;
 
     private ListView listView;
+
+    private Filter filter;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -131,6 +148,124 @@ public class MainActivity extends AppCompatActivity implements NavigationDrawerC
             mNavigationDrawerFragment.closeDrawer();
         else
             super.onBackPressed();
+    }
+    private class UpdateList extends AsyncTask<String, Void, LinkedList<AdapterDetailedList.FileDetail>> {
+
+        String exceptionMessage;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        protected LinkedList<AdapterDetailedList.FileDetail> doInBackground(final String... params) {
+            try {
+
+                final String path = params[0];
+                if (TextUtils.isEmpty(path)) {
+                    return null;
+                }
+
+                File tempFolder = new File(path);
+                if (tempFolder.isFile()) {
+                    tempFolder = tempFolder.getParentFile();
+                }
+
+                String[] unopenableExtensions = {"apk", "mp3", "mp4", "png", "jpg", "jpeg"};
+
+                final LinkedList<AdapterDetailedList.FileDetail> fileDetails = new LinkedList<>();
+                final LinkedList<AdapterDetailedList.FileDetail> folderDetails = new LinkedList<>();
+                currentFolder = tempFolder.getAbsolutePath();
+
+                if (!tempFolder.canRead()) {
+                    if (RootFW.connect()) {
+                        com.spazedog.lib.rootfw4.utils.File folder = RootFW.getFile(currentFolder);
+                        com.spazedog.lib.rootfw4.utils.File.FileStat[] stats = folder.getDetailedList();
+
+                        if (stats != null) {
+                            for (com.spazedog.lib.rootfw4.utils.File.FileStat stat : stats) {
+                                if (stat.type().equals("d")) {
+                                    folderDetails.add(new AdapterDetailedList.FileDetail(stat.name(),
+                                            getString(R.string.folder),
+                                            ""));
+                                } else if (!FilenameUtils.isExtension(stat.name().toLowerCase(), unopenableExtensions)
+                                        && stat.size() <= 20_000 * FileUtils.ONE_KB) {
+                                    final long fileSize = stat.size();
+                                    //SimpleDateFormat format = new SimpleDateFormat("MMM dd, yyyy  hh:mm a");
+                                    //String date = format.format("");
+                                    fileDetails.add(new AdapterDetailedList.FileDetail(stat.name(),
+                                            FileUtils.byteCountToDisplaySize(fileSize), ""));
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    File[] files = tempFolder.listFiles();
+
+                    Arrays.sort(files, getFileNameComparator());
+
+                    if (files != null) {
+                        for (final File f : files) {
+                            if (f.isDirectory()) {
+                                folderDetails.add(new AdapterDetailedList.FileDetail(f.getName(),
+                                        getString(R.string.folder),
+                                        ""));
+                            } else if (f.isFile()
+                                    && !FilenameUtils.isExtension(f.getName().toLowerCase(), unopenableExtensions)
+                                    && FileUtils.sizeOf(f) <= 20_000 * FileUtils.ONE_KB) {
+                                final long fileSize = f.length();
+                                SimpleDateFormat format = new SimpleDateFormat("MMM dd, yyyy  hh:mm a");
+                                String date = format.format(f.lastModified());
+                                fileDetails.add(new AdapterDetailedList.FileDetail(f.getName(),
+                                        FileUtils.byteCountToDisplaySize(fileSize), date));
+                            }
+                        }
+                    }
+                }
+
+                folderDetails.addAll(fileDetails);
+                return folderDetails;
+            } catch (Exception e) {
+                exceptionMessage = e.getMessage();
+                return null;
+            }
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        protected void onPostExecute(final LinkedList<AdapterDetailedList.FileDetail> names) {
+            if (names != null) {
+                boolean isRoot = currentFolder.equals("/");
+                AdapterDetailedList mAdapter = new AdapterDetailedList(getBaseContext(), names, isRoot);
+                listView.setAdapter(mAdapter);
+                filter = mAdapter.getFilter();
+            }
+            if (exceptionMessage != null) {
+                Toast.makeText(MainActivity.this, exceptionMessage, Toast.LENGTH_SHORT).show();
+            }
+            invalidateOptionsMenu();
+            super.onPostExecute(names);
+        }
+
+        public final Comparator<File> getFileNameComparator() {
+            return new AlphanumComparator() {
+                /**
+                 * {@inheritDoc}
+                 */
+                @Override
+                public String getTheString(Object obj) {
+                    return ((File) obj).getName()
+                            .toLowerCase();
+                }
+            };
+        }
     }
 
 }
