5b77d08032fdb2306ad248f587b12373034f2173
vavill <kxzxxx@gmail.com>
2015-10-07 05:13:32 +0800

    Update

---
+++ app/src/main/java/me/myfilemanager/Adapter/NavigationDrawerAdapter.java
@@ -0,0 +1,79 @@
+package me.myfilemanager.Adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import me.myfilemanager.NavigationDrawerCallbacks;
+import me.myfilemanager.Utils.NavigationItem;
+import me.myfilemanager.R;
+
+/**
+ * Created by vV on 2015/6/16.
+ */
+public class NavigationDrawerAdapter extends RecyclerView.Adapter<NavigationDrawerAdapter.ViewHolder> {
+
+    private List<NavigationItem> mData;
+    private NavigationDrawerCallbacks mNavigationDrawerCallbacks;
+    private int mSelectedPosition;
+    // private int mTouchedPosition = -1;
+
+    public NavigationDrawerAdapter(List<NavigationItem> data) {
+        mData = data;
+    }
+
+    public void setNavigationDrawerCallbacks(NavigationDrawerCallbacks navigationDrawerCallbacks) {
+        mNavigationDrawerCallbacks = navigationDrawerCallbacks;
+    }
+
+    @Override
+    public NavigationDrawerAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.drawer_row, viewGroup, false);
+        final ViewHolder vH = new ViewHolder(v);
+        vH.itemView.setOnClickListener(new View.OnClickListener() {
+                                           @Override
+                                           public void onClick(View v) {
+                                               if (mNavigationDrawerCallbacks != null)
+                                                   mNavigationDrawerCallbacks.onNavigationDrawerItemSelected(vH.getAdapterPosition());
+                                           }
+                                       }
+        );
+
+        return vH;
+    }
+
+    @Override
+    public void onBindViewHolder(NavigationDrawerAdapter.ViewHolder viewHolder, final int i) {
+        viewHolder.textView.setText(mData.get(i).getText());
+        viewHolder.textView.setCompoundDrawablesWithIntrinsicBounds(mData.get(i).getDrawable(), null, null, null);
+
+
+
+    }
+
+    public void selectPosition(int position) {
+        int lastPosition = mSelectedPosition;
+        mSelectedPosition = position;
+        notifyItemChanged(lastPosition);
+        notifyItemChanged(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData != null ? mData.size() : 0;
+    }
+
+    protected static class ViewHolder extends RecyclerView.ViewHolder {
+         final TextView textView;
+
+        public ViewHolder(View v) {
+            super(v);
+            textView = (TextView) v.findViewById(R.id.item_name);
+
+        }
+    }
+}
