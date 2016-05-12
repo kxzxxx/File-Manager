54a25333bbd5a249c769b900ec08109205604095
Kevin McKee <kxzxxx@gmail.com>
2016-05-12 23:56:49 +0800

    update

--- app/src/main/java/me/myfilemanager/Activity/MainActivity.java
+++ app/src/main/java/me/myfilemanager/Activity/MainActivity.java
@@ -1,6 +1,8 @@
 package me.myfilemanager.Activity;
 
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -19,6 +21,8 @@ import android.util.Log;
 import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.DialogAction;
@@ -44,165 +48,22 @@ import me.myfilemanager.Utils.UpdateList;
 
 public class MainActivity extends AppCompatActivity implements NavigationDrawerCallbacks {
     public static String currentFolder;
-    String TAG = MainActivity.class.getSimpleName();
+    public static AdapterDetailedList adapter;
     public ActionMode mActionMode;
     public boolean actionMode = false;
-    public static AdapterDetailedList adapter;
-    int mOldStatusBarColor;
     public
     Toolbar ab;
+        NavigationDrawerFragment mNavigationDrawerFragment;
+    MainActivityHandler uiHandler = new MainActivityHandler(this);
     public String sourceLocation;
-
-    LinkedList<String> pathSet = new LinkedList<>();
     public
     RecyclerView recyclerView;
     public Handler chHandler;
-
-    NavigationDrawerFragment mNavigationDrawerFragment;
-
-    static class MainActivityHandler extends Handler {
-        final WeakReference<MainActivity> mTarget;
-
-        MainActivityHandler(MainActivity target) {
-            mTarget = new WeakReference<>(target);
-        }
-
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-           /* switch (msg.what) {
-                case 111: {
-
-
-
-                    break;
-                }
-
-                case (1):
-                    Toast.makeText(mTarget.get().getApplicationContext(), "move file " +
-                                    "successful",
-                            Toast.LENGTH_SHORT).show();
-                    break;
-                default:
-
-                    Toast.makeText(mTarget.get().getApplicationContext(), Integer.toString(msg
-                                    .arg1),
-                            Toast.LENGTH_SHORT).show();
-            }*/
-        }
-    }
-
-    MainActivityHandler uiHandler = new MainActivityHandler(this);
-
-    protected void onCreate(Bundle savedInstanceState) {
-        currentFolder = Environment.getExternalStorageDirectory().getAbsolutePath();
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mOldStatusBarColor = getWindow().getStatusBarColor();
-
-
-        ab = (Toolbar) findViewById(R.id.toolbar_actionbar);
-        recyclerView = (RecyclerView) findViewById(R.id.list);
-        //setup toolbar
-        setSupportActionBar(ab);
-        ab.setOnMenuItemClickListener(onMenuItemClick);
-        Log.d(TAG, "====load====");
-
-        //setup drawer
-
-        mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager()
-                .findFragmentById(
-                        R.id.fragment_drawer);
-        mNavigationDrawerFragment.setup(R.id.fragment_drawer, (CustomDrawer) findViewById(R.id
-                .drawer
-        ), ab);
-
-
-        adapter=new AdapterDetailedList(this,new LinkedList<AdapterDetailedList.FileDetail>());
-        //setup recyclerView
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.addItemDecoration(
-                new HorizontalDividerItemDecoration.Builder(this)
-                        .color(Color.LTGRAY)
-                        .sizeResId(R.dimen.divider)
-                        .marginResId(R.dimen.leftmargin, R.dimen.rightmargin)
-                        .build());
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        ((SimpleItemAnimator) recyclerView.getItemAnimator()).setSupportsChangeAnimations(false);
-
-        // recyclerView.setAdapter(mAdapter); //   new UpdateList(this).execute(currentFolder);
-        //  recyclerView.setTextFilterEnabled(true); //获取主储存路径 //    String homePath =
-        // Environment.getExternalStorageDirectory().getAbsolutePath(); //  File file = new File
-        // (homePath);
-
-        this.onNavigationDrawerItemSelected(0);
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    Toolbar.OnMenuItemClickListener onMenuItemClick = new Toolbar.OnMenuItemClickListener() {
-
-        public boolean onMenuItemClick(MenuItem item) {
-            int id = item.getItemId();
-            //noinspection SimplifiableIfStatement
-            if (id == R.id.action_settings) {
-                Intent intent = new Intent(MainActivity.this, SettingActivity.class);
-                MainActivity.this.startActivity(intent);
-                return true;
-            }
-
-            if (id == R.id.pastefile) {
-                //TODO:add a dialog , notify
-
-                final String[] pathS =
-                pathSet.toArray(new String[pathSet.size()]);
-                Log.d(TAG, "paste " + Integer.toString(pathSet.size())+"files");
-
-                new MoveFile(MainActivity.this).execute(pathS);
-
-
-                adapter.notifyDataSetChanged();
-
-                return true;
-            }
-
-            return true;
-        }
-    };
-
-    public void onBackPressed() {
-        //exit action mode
-        if (mNavigationDrawerFragment.isDrawerOpen())
-            mNavigationDrawerFragment.closeDrawer();
-      /* else if(this.mActionMode != null){
-            this.actionMode = false;
-            this.mActionMode.finish();
-            this.mActionMode = null;
-        }*/
-
-        else if (currentFolder.isEmpty() || currentFolder.equals("/")) {
-            Log.d(TAG, "finish");
-            finish();
-        } else {
-            File file = new File(currentFolder);
-            String parentFolder = file.getParent();
-
-            new UpdateList(this).execute(parentFolder);
-        }
-
-
-    }
-
-    public void onStop() {
-        super.onStop();
-        NavigationDrawerFragment.readSharedSetting(this, "currentFolder", currentFolder);
-    }
-
+    String TAG = MainActivity.class.getSimpleName();
+    int mOldStatusBarColor;
+    View mRevealView;
+    View mRevealBackgroundView;
+    LinkedList<String> pathSet = new LinkedList<>();
     public ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
 
         // Called when the action mode is created; startActionMode() was called
@@ -210,7 +71,7 @@ public class MainActivity extends AppCompatActivity implements NavigationDrawerC
         public boolean onCreateActionMode(ActionMode mode, Menu menu) {
             // Inflate a menu resource providing context menu items
 
-            getMenuInflater().inflate(R.menu.filehandler, menu);
+             getMenuInflater().inflate(R.menu.filehandler, menu);
 
             return true;
         }
@@ -220,9 +81,9 @@ public class MainActivity extends AppCompatActivity implements NavigationDrawerC
         @Override
         public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
 
-            getWindow().setStatusBarColor(getResources().getColor(R.color
-                    .myactionModePrimaryDarkColor));
-
+            //  getWindow().setStatusBarColor(getResources().getColor(R.color
+            //         .myactionModePrimaryDarkColor));
+            animateAppAndStatusBar(R.color.myPrimaryColor, R.color.myactionModePrimaryColor);
             pathSet.clear();
             return false; // Return false if nothing is done
         }
@@ -279,11 +140,144 @@ public class MainActivity extends AppCompatActivity implements NavigationDrawerC
 
 
             }
-            getWindow().setStatusBarColor(mOldStatusBarColor);
+            animateAppAndStatusBar(R.color.myactionModePrimaryColor, R.color.myPrimaryColor);
             actionMode = false;
             mActionMode = null;
         }
     };
+
+    Toolbar.OnMenuItemClickListener onMenuItemClick = new Toolbar.OnMenuItemClickListener() {
+
+        public boolean onMenuItemClick(MenuItem item) {
+            int id = item.getItemId();
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+                MainActivity.this.startActivity(intent);
+                return true;
+            }
+
+            if (id == R.id.pastefile) {
+                //TODO:add a dialog , notify
+
+                final String[] pathS =
+                        pathSet.toArray(new String[pathSet.size()]);
+                Log.d(TAG, "paste " + Integer.toString(pathSet.size()) + "files");
+
+                new MoveFile(MainActivity.this).execute(pathS);
+
+
+                adapter.notifyDataSetChanged();
+
+                return true;
+            }
+
+            return true;
+        }
+    };
+
+    protected void onCreate(Bundle savedInstanceState) {
+        currentFolder = Environment.getExternalStorageDirectory().getAbsolutePath();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mOldStatusBarColor = getWindow().getStatusBarColor();
+
+
+        ab = (Toolbar) findViewById(R.id.appbar);
+        recyclerView = (RecyclerView) findViewById(R.id.list);
+        mRevealView = findViewById(R.id.reveal);
+        mRevealBackgroundView = findViewById(R.id.revealBackground);
+        //setup toolbar
+        setSupportActionBar(ab);
+        ab.setOnMenuItemClickListener(onMenuItemClick);
+        Log.d(TAG, "====load====");
+
+        //setup drawer
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager()
+                .findFragmentById(
+                        R.id.fragment_drawer);
+        mNavigationDrawerFragment.setup(R.id.fragment_drawer, (CustomDrawer) findViewById(R.id
+                .drawer
+        ), ab);
+
+
+        adapter = new AdapterDetailedList(this, new LinkedList<AdapterDetailedList.FileDetail>());
+        //setup recyclerView
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+     /*   recyclerView.addItemDecoration(
+                new HorizontalDividerItemDecoration.Builder(this)
+                        .color(Color.LTGRAY)
+                        .sizeResId(R.dimen.divider)
+                        .marginResId(R.dimen.leftmargin, R.dimen.rightmargin)
+                        .build());*/
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        ((SimpleItemAnimator) recyclerView.getItemAnimator()).setSupportsChangeAnimations(false);
+
+        // recyclerView.setAdapter(mAdapter); //   new UpdateList(this).execute(currentFolder);
+        //  recyclerView.setTextFilterEnabled(true); //获取主储存路径 //    String homePath =
+        // Environment.getExternalStorageDirectory().getAbsolutePath(); //  File file = new File
+        // (homePath);
+
+        this.onNavigationDrawerItemSelected(0);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void onBackPressed() {
+        //exit action mode
+        if (mNavigationDrawerFragment.isDrawerOpen())
+            mNavigationDrawerFragment.closeDrawer();
+      /* else if(this.mActionMode != null){
+            this.actionMode = false;
+            this.mActionMode.finish();
+            this.mActionMode = null;
+        }*/
+
+        else if (currentFolder.isEmpty() || currentFolder.equals("/")) {
+            Log.d(TAG, "finish");
+            finish();
+        } else {
+            File file = new File(currentFolder);
+            String parentFolder = file.getParent();
+
+            new UpdateList(this).execute(parentFolder);
+        }
+
+
+    }
+
+    public void onStop() {
+        super.onStop();
+        NavigationDrawerFragment.readSharedSetting(this, "currentFolder", currentFolder);
+    }
+
+    private void animateAppAndStatusBar(int fromColor, final int toColor) {
+        Animator animator = ViewAnimationUtils.createCircularReveal(
+                mRevealView,
+                ab.getWidth() / 2,
+                ab.getHeight() / 2, 0,
+                ab.getWidth() / 2);
+
+        animator.addListener(new AnimatorListenerAdapter() {
+
+            public void onAnimationStart(Animator animation) {
+                mRevealView.setBackgroundColor(getResources().getColor(toColor));
+            }
+        });
+
+        mRevealBackgroundView.setBackgroundColor(getResources().getColor(fromColor));
+        animator.setStartDelay(200);
+        animator.setDuration(125);
+        animator.start();
+        mRevealView.setVisibility(View.VISIBLE);
+    }
 
     public void onNavigationDrawerItemSelected(int itemPosition) {
 
@@ -292,6 +286,37 @@ public class MainActivity extends AppCompatActivity implements NavigationDrawerC
                 (itemPosition).getText());
 
 
+    }
+
+    static class MainActivityHandler extends Handler {
+        final WeakReference<MainActivity> mTarget;
+
+        MainActivityHandler(MainActivity target) {
+            mTarget = new WeakReference<>(target);
+        }
+
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+           /* switch (msg.what) {
+                case 111: {
+
+
+
+                    break;
+                }
+
+                case (1):
+                    Toast.makeText(mTarget.get().getApplicationContext(), "move file " +
+                                    "successful",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+
+                    Toast.makeText(mTarget.get().getApplicationContext(), Integer.toString(msg
+                                    .arg1),
+                            Toast.LENGTH_SHORT).show();
+            }*/
+        }
     }
 
 
